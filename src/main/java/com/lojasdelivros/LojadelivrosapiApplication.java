522c840d7a589326309ebd4e0d555bbecac9d4a0
Getulio222 <getulio222@hotmail.com>
2025-11-11 18:19:59 -0300

    Commit inicial, criacao do metodo de teste REST

---
+++ src/main/java/com/lojasdelivros/LojadelivrosapiApplication.java
@@ -0,0 +1,13 @@
+package com.lojasdelivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojadelivrosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojadelivrosapiApplication.class, args);
+	}
+
+}
